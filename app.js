584103fc73f97d1114727a4b36c2bb9033e653f0
Khushi-Malik <khushimalik1910@gmail.com>
2025-12-26 19:23:47 -0500

    mongo connected

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db');
+
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+
+// Connect to Database
+connectDB();
 
 app.use(express.static('public'));
 
@@ -14,10 +20,6 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
-
-app.get('', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 // Routes
-router.get('', (req, res) => {
+
+// GET
+// HOME
+router.get('', async (req, res) => {
     const locals = {
         title: "Unhidden",
         description: "Welcome to the Unhidden Blog"
 
     }
-  res.render('index', { locals });
+
+    try {
+      const data = await Post.find();
+      res.render('index', { locals, data });
+    } catch (error) {
+      console.error(error);
+    }
 });
 
-router.get('\about', (req, res) => {
+
+// GET
+// Post:id
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: data.body
+    }
+
+    res.render('post', { locals, data });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+// GET
+// Post: SearchTerm
+router.get('/search', async (req, res) => {
+  try {
+    let searchTerm = req.query.searchTerm;
+
+    const data = await Post.find({ title: { $regex: searchTerm, $options: 'i' } });
+
+    const locals = {
+      title: "Search Results",
+      description: "Search the site"
+    }
+
+    res.render('post', { locals, data });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// function insertPostData() {
+//   Post.insertMany([
+//     {
+//       title:"First Post",
+//       body:"This is the body of the first post."
+//     }
+//   ])
+// }
+// insertPostData(); 
+
+
+router.get('/about', (req, res) => {
   res.render('about');
 });
 
