647734d8e0c450cff35e8a934cd5835ab4c0e1ec
Khushi-Malik <khushimalik1910@gmail.com>
2026-01-08 18:18:01 -0500

    deplouyment2

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URL {
-      dbName: 'blog', // IMPORTANT
+  try {
+    mongoose.set('strictQuery', false);
+
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: 'blog',
     });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("Database connection error:", error);
-    }
-}
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('Database connection error:', error.message);
+    process.exit(1); // ⬅️ CRITICAL
+  }
+};
 
 module.exports = connectDB;
