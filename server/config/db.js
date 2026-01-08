599864f054a043e8ea10920f9613fe65f140c5b8
Khushi-Malik <khushimalik1910@gmail.com>
2026-01-08 18:04:04 -0500

    trying to fix deployment bugs

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URL)
+        const conn = await mongoose.connect(process.env.MONGODB_URL {
+      dbName: 'blog', // IMPORTANT
+    });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("Database connection error:", error);
